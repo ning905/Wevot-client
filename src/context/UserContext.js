9f25923d09de15131ed08fc11ab25e73d3a01392
Ning Ma <ningma905@gmail.com>
2022-10-20 18:13:39 +0100

    feat: userContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,15 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useReducer } from 'react'
+import jwt_decode from 'jwt-decode'
+import client from '../utils/client'
+import UserReducer from './userReducer'
 
-export const UserContext = createContext()
- 
-export const UserContextProvider = ({children}) => {
- const [user, setuser] = useState({});
+const tokenKey = process.env.REACT_APP_USER_TOKEN
+const token = localStorage.getItem(tokenKey) || null
 
- 
-    return (
-         <UserContext.Provider value={user}>
-            {children}
-         </UserContext.Provider>
-    );
- 
+const INITIAL_USER = null
+export const UserContext = createContext(INITIAL_USER)
+
+export const UserContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(UserReducer, { user: INITIAL_USER })
+
+  useEffect(() => {
+    if (token) {
+      const username = jwt_decode(token).username
+      client
+        .get(`/users/${username}`)
+        .then((res) => dispatch({ type: 'LOGIN', payload: res.data.data }))
+        .catch((err) => {
+          console.log('remove token')
+          localStorage.removeItem(tokenKey)
+          console.error(err)
+        })
+    }
+  }, [])
+
+  return (
+    (!token || state.user) && (
+      <UserContext.Provider value={{ currentUser: state.user, userAction: dispatch }}>
+        {children}
+      </UserContext.Provider>
+    )
+  )
 }
---
+++ src/context/userReducer.js
@@ -0,0 +1,19 @@
+const UserReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN': {
+      return {
+        user: action.payload,
+      }
+    }
+
+    case 'LOGOUT': {
+      return {
+        user: null,
+      }
+    }
+    default:
+      return state
+  }
+}
+
+export default UserReducer
