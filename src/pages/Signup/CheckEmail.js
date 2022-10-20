fccf62ffcda9a1417b5aa5d9dfe4913d3fcb1788
Ning Ma <ningma905@gmail.com>
2022-10-20 18:14:08 +0100

    feat: resend email function

--- src/pages/Signup/CheckEmail.js
+++ src/pages/Signup/CheckEmail.js
@@ -1,7 +1,32 @@
 import './signup.scss'
 import Navbar from '../../components/navbar/Navbar'
+import { useParams } from 'react-router-dom'
+import client from '../../utils/client'
+import { useState } from 'react'
+
+const initAlert = { status: '', content: '' }
 
 export default function CheckEmail() {
+  const { username } = useParams()
+  const [alert, setAlert] = useState(initAlert)
+
+  function handleResend() {
+    client
+      .post(`/users/signup/resend-email/${username}`)
+      .then((res) => {
+        setAlert({ status: 'success', content: res.data.message })
+        setTimeout(() => {
+          setAlert(initAlert)
+        }, 5000)
+      })
+      .catch((res) => {
+        setAlert({ status: 'error', content: res.response.data.message })
+        setTimeout(() => {
+          setAlert(initAlert)
+        }, 5000)
+      })
+  }
+
   return (
     <div className='signup'>
       <Navbar page='home' />
@@ -12,8 +37,15 @@ export default function CheckEmail() {
             <h2>Check Your Email</h2>
             <p>We have sent you an email with a link to active your account.</p>
             <p>Please check your email and spam folder.</p>
-            <p className='resend'>
-              Can’t find the email? <span className='clickable'>Click here to resend</span>
+
+            {alert.status === 'success' && <p id='success-alert'>Success: {alert.content}</p>}
+            {alert.status === 'error' && <p id='fail-alert'>Failed: {alert.content}</p>}
+
+            <p className='reminder'>
+              Can’t find the email?{' '}
+              <span onClick={handleResend} className='clickable'>
+                Click here to resend
+              </span>
             </p>
           </div>
         </div>
