86d4b2601111013e459789c5ea9c61b1e723ff32
Ning Ma <ningma905@gmail.com>
2022-10-26 17:59:15 +0100

    refactor: join event form

--- src/components/joinForm/JoinForm.js
+++ src/components/joinForm/JoinForm.js
@@ -2,9 +2,10 @@ import './joinForm.scss'
 import { Dialog, DialogActions, DialogContent, TextField } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { getCustomInputStyles } from '../../utils/muiCustomTheme'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { areAllFieldsValid, error } from '../../utils/validRegex'
 import client from '../../utils/client'
+import { UserContext } from '../../context/UserContext'
 
 const initInputAlert = {
   code: { status: '', content: ' ' },
@@ -19,6 +20,11 @@ const initInputs = {
 }
 
 export default function JoinForm({ open, setOpen }) {
+  const { currentUser } = useContext(UserContext)
+  if (currentUser) {
+    initInputs.email = currentUser.email
+  }
+
   const [inputs, setInputs] = useState(initInputs)
   const [alert, setAlert] = useState(initInputAlert)
   const [resErr, setResErr] = useState('')
@@ -44,73 +50,75 @@ export default function JoinForm({ open, setOpen }) {
   }
 
   function handleJoin() {
-    console.log('inputs: ', inputs)
     if (areAllFieldsValid(initInputs, inputs, alert, setAlert)) {
-      console.log('all valid')
       client
         .get(`/events/participate/${inputs.code}`)
         .then((res) => {
-          console.log('response: ', res)
           if (res.data.status === 'success') {
-            navigate(`/events/participate/${inputs.code}`)
+            navigate(`/events/participate/${inputs.code}`, { state: { userInfo: inputs } })
           }
         })
         .catch((res) => {
-          console.log('response: ', res.response.data)
-          const data = res.response.data
-          setResErr(data.message)
+          setResErr(res.response.data.message)
         })
     }
   }
 
   return (
-    <div className='join-form'>
-      <Dialog open={open} onClose={handleClose}>
+    <Dialog open={open} onClose={handleClose} className='join-form-dialog'>
+      <div className='text-wrap'>
         <h2>Please provide the event code</h2>
-        {resErr && <p>Failed: {resErr}</p>}
-        <DialogContent>
-          <TextField
-            required
-            className='input-field'
-            variant='standard'
-            label='EVENT CODE'
-            type='text'
-            name='code'
-            value={inputs.code}
-            color={alert.code.status}
-            helperText={alert.code.content}
-            sx={getCustomInputStyles(alert.code)}
-            onChange={handleInput}
-          />
-          <TextField
-            required
-            className='input-field'
-            variant='standard'
-            label='EMAIL'
-            type='text'
-            name='email'
-            value={inputs.email}
-            color={alert.email.status}
-            helperText={alert.email.content}
-            sx={getCustomInputStyles(alert.email)}
-            onChange={handleInput}
-          />
-          <TextField
-            className='input-field'
-            variant='standard'
-            label='NAME'
-            type='text'
-            name='name'
-            value={inputs.name}
-            sx={getCustomInputStyles()}
-            onChange={handleInput}
-          />
-        </DialogContent>
+        {resErr && <p className='error'>Failed: {resErr}</p>}
+      </div>
 
-        <DialogActions>
-          <button onClick={handleJoin}>JOIN</button>
-        </DialogActions>
-      </Dialog>
-    </div>
+      <DialogContent className='inputs-wrap'>
+        <TextField
+          required
+          className='input-field'
+          variant='standard'
+          label='EVENT CODE'
+          type='text'
+          name='code'
+          value={inputs.code}
+          color={alert.code.status}
+          helperText={alert.code.content}
+          sx={getCustomInputStyles(alert.code)}
+          onChange={handleInput}
+        />
+        {!currentUser && (
+          <>
+            <TextField
+              required
+              className='input-field'
+              variant='standard'
+              label='EMAIL'
+              type='text'
+              name='email'
+              value={inputs.email}
+              color={alert.email.status}
+              helperText={alert.email.content}
+              sx={getCustomInputStyles(alert.email)}
+              onChange={handleInput}
+            />
+          </>
+        )}
+        <TextField
+          className='input-field'
+          variant='standard'
+          label='NAME'
+          type='text'
+          name='name'
+          value={inputs.name}
+          sx={getCustomInputStyles()}
+          onChange={handleInput}
+        />
+      </DialogContent>
+
+      <DialogActions className='btn-wrap'>
+        <button onClick={handleJoin} className='join-btn'>
+          JOIN
+        </button>
+      </DialogActions>
+    </Dialog>
   )
 }
